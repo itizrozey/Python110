e9f12df786bc41448ecf1d087ea592504f102c6b
itizrozey <114096796+itizrozey@users.noreply.github.com>
2022-09-21 17:39:19 +0100

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,46 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:/Users/User/Documents/Python/day1.py ===============
+hello world
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+python is a programming language
+pyhton is easy
+and python is fun
+python is a programming language
+pyhton is easy
+python is fun
+hello world
+my name is taiwo
+paul taiwo
+I sold a pair of shoe for 25000, 3 shirts for 15000 and a suit for 45000
+I sold a pair of shoe for 25000, 3 shirts for 15000 and a suit for 45000
+PYHTON
+python
+Python Is Fun
+Python is fun
+['python', 'is', 'fun']
+info
+
+================ RESTART: C:/Users/User/Documents/Python/day1.py ===============
+hello world
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+python is a programming language
+pyhton is easy
+and python is fun
+python is a programming language
+pyhton is easy
+python is fun
+hello world
+my name is taiwo
+paul taiwo
+I sold a pair of shoe for 25000, 3 shirts for 15000 and a suit for 45000
+I sold a pair of shoe for 25000, 3 shirts for 15000 and a suit for 45000
+PYHTON
+python
+Python Is Fun
+Python is fun
+['python', 'is', 'fun']
+info
